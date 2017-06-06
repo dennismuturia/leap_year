20ba42cb7cc8a960e793c8efa44737c1c3f8952e
dennis292 <dennis292@localhost.localdomain>
2017-06-06 15:50:07 +0300

    Tested on the leap year

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
+      if (year%100 === 0) {
         return false;
+      }
+      else if(year%4 === 0){
+          return true;
+        }else {
+          return false;
+        }
       };
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
