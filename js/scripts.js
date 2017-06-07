8a622f170a3174522430b1bdb35b881123bd05a9
dennis292 <dennis292@localhost.localdomain>
2017-06-07 08:11:29 +0300

    Added a few modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
 
+//This is the business logic
+var leapYear = function(year) {
+  if ((year%4 === 0) && (year%100 != 0) || (year%400 === 0)) {
+    return true;
+  }
+    else {
+      return false;
+    }
+  }
+//Now lets start the user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-    var leapYear = function(year) {
-      if (year%100 === 0) {
-        return false;
-      }
-      else if(year%4 === 0){
-          return true;
-        }else {
-          return false;
-        }
-      };
+
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+        $(".year").text(year);
+
+        if (!result) {                 // same as writing if (result === false)
+          $(".not").text("not");
+        } else {
+          $(".not").text("");
+        }
+
+        $("#result").show();
   });
 });
